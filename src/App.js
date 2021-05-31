d713864eaa5d02837ca5e69ad59748d91881167b
herambhakumar.p <herambhakumar.p@inteqsolutions.com>
2021-05-31 12:47:09 +0530

    Herambha checkin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <p>nagesh</p>
-	  <p>{new Date().toLocaleString() + ''}</p>
+      <p>{new Date().toLocaleString() + ""}</p>
+      <h1>Herambha</h1>
     </div>
   );
 }
